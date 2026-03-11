88959607b84c515506d1f0bd288aa84d24f46b5c
Rashi Dang <rashidang12@gmail.com>
2026-03-11 18:07:39 +0000

    p33.py

---
+++ p33.py
@@ -0,0 +1,7 @@
+#Question: Assign two different string values to variables first_name and last_name,
+#then use the + operator to combine them into a new variable called full_name with a space in between. 
+#Print full_name. 
+s1 =" rashi"
+s2 = "dang"
+s3 = s1+s2
+print (s3)
