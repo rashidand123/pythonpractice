c8e4dae23f718cb36931e531c9d9cd699747ecc5
Rashi Dang <rashidang12@gmail.com>
2026-03-20 17:59:43 +0000

    p51.py

---
+++ p51.py
@@ -0,0 +1,15 @@
+#string functions 
+#ends with
+str="I am studying"
+print(str.endswith("ing"))
+
+str="I am studying"
+print(str.endswith("stu"))
+
+#capitalize
+str="i am studying"
+print(str.capitalize())
+
+str="i am studying"
+str=str.capitalize()
+print(str)
