702fcdd1660b830c60ef546ddac07bb4efb5251d
Rashi Dang <rashidang12@gmail.com>
2026-03-25 20:16:20 +0530

    Add marks and student lists with basic operations

---
+++ p63.py
@@ -0,0 +1,12 @@
+marks=[93.5,56.8,94.7,45.6,85.9]
+
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks))
+
+student=["karan", 17.5, 56.8, "Delhi"]
+print(student[0])
+student[0]= "arjun"
+print(student)
