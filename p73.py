4d74961e741278861aa62021edfd6bffcfdcca5b
Rashi Dang <rashidang12@gmail.com>
2026-03-30 16:48:39 +0000

    p73.py

---
+++ p73.py
@@ -0,0 +1,11 @@
+#nested dictionary
+student = {
+    "name" : "rashi",
+    "subjects" : {
+        "eco" : 88,
+        "acc" :90,
+        "bst" : 86
+     }
+}
+print(student)
+print(student["subjects"]["acc"])
