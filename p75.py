59b8551450b0b21bfc5396407f64ea1be1eee23c
Rashi Dang <rashidang12@gmail.com>
2026-03-31 15:15:20 +0000

    p75.py

---
+++ p75.py
@@ -0,0 +1,11 @@
+#sets
+collection={1,2,3,4,"hello",2,4,"hello"}
+
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection={} #empty dict
+collection = set() #empty set ;syntax
+
+print(type(collection))
