ee11ca0a2984e4be6da0053b6a3e3474e14f295d
Rashi Dang <rashidang12@gmail.com>
2026-04-04 17:07:32 +0530

    Create p83.py
    Added loops to print 'hello' and numbers from 1 to 5.

---
+++ p83.py
@@ -0,0 +1,17 @@
+#loops
+
+count=1
+while count <= 5:
+  print("hello")
+  count += 1
+
+i = 1
+while count <= 5:
+  print("hello")
+  i += 1
+
+#print numbers from 1 to 5
+i = 1
+while i <= 5:
+  print(i)
+  i+=1
