0850c5aac5c83a7d6692374ce85501a4cc2e22ba
Rashi Dang <rashidang12@gmail.com>
2026-04-07 15:07:02 +0000

    p90.py

---
+++ p90.py
@@ -0,0 +1,24 @@
+#pass statement 
+#for i in range (5):
+ #   pass 
+
+#print ("some useful work")    
+
+#write a program to find the sum of n numbers .(using while)
+
+n =5 
+
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + i
+
+print("total sum:", sum)
+
+# write a program to find the fractional of first n numbers . (using for)
+
+n=5
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+
+print("factorial :", fact)
