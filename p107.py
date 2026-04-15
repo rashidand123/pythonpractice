d63886047ed1bcf3e8456ddc8b5a2ec0e2aa5686
Rashi Dang <rashidang12@gmail.com>
2026-04-15 15:07:25 +0000

    p107.py

---
+++ p107.py
@@ -0,0 +1,7 @@
+# Create a variable score = 50 and another variable Score = 100. Print both to demonstrate that Python is case-sensitive.
+
+score = 50
+Score = 100
+
+print("Small score:", score)
+print("Capital Score:", Score)
