35f74c4952122a25261c0ad03106261b0e64aeb8
Rashi Dang <rashidang12@gmail.com>
2026-04-16 16:49:59 +0000

    p108.py

---
+++ p108.py
@@ -0,0 +1,5 @@
+# Write a script to calculate the total sum of all numerical values in a dictionary.
+
+scores = {'math': 90, 'science': 85, 'history': 75}
+total = sum(scores.values())
+print(total)
