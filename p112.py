4029004479d3feb40c9ba576fe508c1f5637d97c
Rashi Dang <rashidang12@gmail.com>
2026-04-18 09:40:07 +0000

    p112.py

---
+++ p112.py
@@ -0,0 +1,22 @@
+#fuctions and recursions 
+
+def calc_sum(a,b):
+    sum = a+b
+    print(sum)
+    return sum 
+
+#calc_sum (5,10)
+
+calc_sum (6,8)
+
+calc_sum (9,17)
+
+#average of 3 numbers 
+
+def calc_avg (a, b, c):
+    sum = a+b+c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg(1, 2, 3)
