5bc33efa6aeb19d37acabb0fdaf9be2a2f1518b5
Rashi Dang <rashidang12@gmail.com>
2026-04-26 13:04:31 +0000

    p130.py

---
+++ p130.py
@@ -0,0 +1,9 @@
+#Write a program to find the factorial of a number using a loop.
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+print(factorial(5))
