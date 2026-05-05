aef1876a603d7b85ca4ccc491a23654ce9dc18a3
Rashi Dang <rashidang12@gmail.com>
2026-05-05 18:00:50 +0000

    p152.py

---
+++ p152.py
@@ -0,0 +1,6 @@
+# Calculate the total sum of all values in the dictionary scores = {"math": 90, "science": 85, "english": 78}.
+
+scores = {"math": 90, "science": 85, "english": 78}
+
+total = sum(scores.values())
+print(total)
